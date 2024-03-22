8e98e31327782c2c51b776bcbda3c96482dc8302
Kelechi100 <kintucis6@gmail.com>
2024-03-22 16:19:49 +0200

    Features runs in the mainservice

--- src/main/MainService.java
+++ src/main/MainService.java
@@ -1,10 +1,17 @@
 package main;
 
+import model.Bus;
+import model.Train;
+
 public class MainService {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Bus b1 = new Bus();
+		System.out.println(b1);
+		Train t1 = new Train();
+		System.out.println(t1);
+		
 	}
 
 }
