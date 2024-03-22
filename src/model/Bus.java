be37191ecd9f9808b4cfa2411ca79ab2e54e11a7
Kelechi100 <kintucis6@gmail.com>
2024-03-22 16:09:46 +0200

    Created a class features bus and trains

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Bus {
-
+public class Bus extends Vehicle{
+	private int numberOfSeat;
+	private boolean hasBaggageDivision;
+	
+	// setters and getters 
+	
+	public int getNumberOfSeat() {
+		return numberOfSeat;
+	}
+	public void setNumberOfSeat(int numberOfSeat) {
+		if(numberOfSeat > 10 && numberOfSeat < 1000)
+			this.numberOfSeat = numberOfSeat;
+		else
+			numberOfSeat = 10;
+	}
+	public boolean isHasBaggageDivision() {
+		return hasBaggageDivision;
+	}
+	public void setHasBaggageDivision(boolean hasBaggageDivision) {
+		this.hasBaggageDivision = hasBaggageDivision;
+	}
+	//  constructors
+	public Bus()
+	{
+		super();
+		setNumberOfSeat(10);
+		setHasBaggageDivision(hasBaggageDivision);
+	}
+	
+	// TOString
+	public String toString()
+	{
+		return super.toString() + " " +  numberOfSeat + " "  + hasBaggageDivision;
+	}
 }
--- src/model/Train.java
+++ src/model/Train.java
@@ -1,5 +1,38 @@
 package model;
 
-public class Train {
-
+public class Train extends Vehicle {
+	private String additionalTechnique;
+	private boolean isOnlyLargeTires;
+	
+	
+	// setters and getters
+	public String getAdditionalTechnique() {
+		return additionalTechnique;
+	}
+	public void setAdditionalTechnique(String additionalTechnique) {
+		if(additionalTechnique != null )
+			this.additionalTechnique = additionalTechnique;
+		else
+			this.additionalTechnique = "it has  3 unique features";
+	}
+	public boolean isOnlyLargeTires() {
+		return isOnlyLargeTires;
+	}
+	public void setOnlyLargeTires(boolean isOnlyLargeTires) {
+		this.isOnlyLargeTires = isOnlyLargeTires;
+	}
+	
+	// contsrtuctors
+	public Train()
+	{
+		super();
+		setAdditionalTechnique(additionalTechnique);
+		setOnlyLargeTires(isOnlyLargeTires);
+	}
+	
+	// ToString
+	public String toString()
+	{
+		return super.toString() + " " + additionalTechnique + " " + isOnlyLargeTires;
+	}
 }
